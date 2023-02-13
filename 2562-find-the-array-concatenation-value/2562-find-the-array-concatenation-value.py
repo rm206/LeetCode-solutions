67da1a31714fadb25596b5303f2f553bed2a3dfa
rm206 <75585601+rm206@users.noreply.github.com>
2023-02-12 22:05:14 -0600

    Time: 59 ms (72.22%), Space: 14.1 MB (27.78%) - LeetHub

---
+++ 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findTheArrayConcVal(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        total = 0
+        l = 0
+        r = len(nums) - 1
+        while l < r:
+            concat = nums[l]
+            temp_pow = 10
+
+            while (temp_pow <= nums[r]):
+                temp_pow *= 10
+
+            concat = (concat * temp_pow) + nums[r]
+            total += concat
+            l += 1
+            r -= 1
+        
+        if l == r:
+            total += nums[l]
+        
+        return total
+                
+        
