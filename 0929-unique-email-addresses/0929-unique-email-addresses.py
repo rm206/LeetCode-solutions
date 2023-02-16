53d81b19996de3022c8328cc70349d6df0587fce
rm206 <75585601+rm206@users.noreply.github.com>
2023-02-15 23:55:06 -0600

    Time: 45 ms (97.38%), Space: 14.1 MB (28.60%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.py
+++ 0929-unique-email-addresses/0929-unique-email-addresses.py
@@ -3,14 +3,8 @@ class Solution:
         unique_mails = set({})
         for curr in emails:
             local, domain = curr.split('@')
-            builder = ''
-            local_to_build = ''
-            for ch in local.split('+')[0]:
-                if ch != '.':
-                    local_to_build += ch
-            builder += local_to_build
-            builder += '@'
-            builder += domain
-            unique_mails.add(builder)
+            local = local.split('+')[0]
+            local = local.replace(".", "")
+            unique_mails.add(local+'@'+domain)
         
         return len(unique_mails)
