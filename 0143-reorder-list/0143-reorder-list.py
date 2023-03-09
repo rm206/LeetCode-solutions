7c2065eb3406e42842335d6312e3f277a30f56a9
Rishabh <75585601+rm206@users.noreply.github.com>
2023-03-08 22:34:39 -0600

    Time: 103 ms (21.47%), Space: 23.9 MB (58.44%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.py
@@ -0,0 +1,59 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = None, head
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+
+        return prev
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        count = 0
+        curr = head
+        while curr:
+            count += 1
+            curr = curr.next
+
+        l1, l2 = None, None
+        if count % 2 == 0:
+            count = count /2
+        else:
+            count = (count // 2) + 1
+        
+        l1 = head
+        temp = head
+        splitCount = 0
+        while splitCount < count-1:
+            temp = temp.next
+            splitCount += 1
+            
+        l2 = temp.next
+        temp.next = None
+        l2 = self.reverseList(l2)
+        
+        dummy = ListNode()
+        tail = dummy
+        nextL = 1
+        while l1 or l2:
+            if nextL == 1:
+                tail.next = l1
+                l1 = l1.next
+                nextL = 2
+            else:
+                tail.next = l2
+                l2 = l2.next
+                nextL = 1
+            tail = tail.next
+        
+        return dummy.next
+        
+                
