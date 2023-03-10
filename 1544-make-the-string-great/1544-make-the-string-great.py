0a9201f0f6001340e19ecb23096527804a575a53
Rishabh <75585601+rm206@users.noreply.github.com>
2023-03-09 23:27:17 -0600

    Time: 38 ms (55.24%), Space: 13.8 MB (49.21%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -4,7 +4,8 @@ class Solution:
         for c in s:
             if stack == []:
                 stack.append(c)
-            elif (stack[-1].lower() == c.lower()) and ((stack[-1].islower() and c.isupper()) or (stack[-1].isupper() and c.islower())):
+            # elif (stack[-1].lower() == c.lower()) and ((stack[-1].islower() and c.isupper()) or (stack[-1].isupper() and c.islower())):
+            elif abs(ord(stack[-1]) - ord(c)) == 32:
                 stack.pop()
             else:
                 stack.append(c)
