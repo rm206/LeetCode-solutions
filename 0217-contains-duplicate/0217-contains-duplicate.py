5df5f9dd5121a9c3b95637d7b56567e32164c3ca
Rishabh <75585601+rm206@users.noreply.github.com>
2023-05-15 23:59:20 -0500

    Time: 522 ms (99.38%), Space: 31.8 MB (9.49%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,29 +1,5 @@
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        # Nov 25
-        # s = set({})
-        # for curr in nums:
-        #     if curr in s:
-        #         return True
-        #     else:
-        #         s.add(curr)
-        # return False
-        # nums.sort()
-        # for i in range(len(nums)-1):
-        #     if nums[i] == nums[i+1]:
-        #         return True
-        # return False
-        
-        #Jan 9
-        # return len(set(nums)) != len(nums)
-        # set_nums = set({})
-        # for num in nums:
-        #     if num in set_nums:
-        #         return True
-        #     else:
-        #         set_nums.add(num)
-        # return False
-        
+    def containsDuplicate(self, nums: List[int]) -> bool:        
         numsSet = set({})
         for n in nums:
             if n in numsSet:
