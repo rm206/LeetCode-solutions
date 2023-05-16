d82492da1b8f9881d2ef5e8a247e436eb4e90505
Rishabh <75585601+rm206@users.noreply.github.com>
2023-05-16 02:43:26 -0500

    Time: 85 ms (6.64%), Space: 16.9 MB (20.99%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,47 +1,19 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # Nov 27, 2022
-        # map1 = {}
-        # for i in s:
-        #     if i not in map1:
-        #         map1[i] = 1
-        #     else:
-        #         map1[i] += 1
+        arrS = [0] * 26
+        arrT = [0] * 26
         
-        # map2 = {}
-        # for i in t:
-        #     if i not in map2:
-        #         map2[i] = 1
-        #     else:
-        #         map2[i] += 1
+        for c in s:
+            arrS[ord(c) - ord('a')] += 1
+        for c in t:
+            arrT[ord(c) - ord('a')] += 1
         
-        # if(len(map1) != len(map2)):
-        #     return False
-        # if(map1.keys() != map2.keys()):
-        #     return False
+        for c in s:
+            arrT[ord(c) - ord('a')] -= 1
         
-        # for i in map1:
-        #     if map1[i] != map2[i]:
-        #         return False
+        for n in arrT:
+            if n != 0:
+                return False
         
-        # return True
-    
-#             if len(s) != len(t):
-#             return False
-
-#         countS, countT = {}, {}
-
-#         for i in range(len(s)):
-#             countS[s[i]] = 1 + countS.get(s[i], 0)
-#             countT[t[i]] = 1 + countT.get(t[i], 0)
-#         return countS == countT
-
-        # Jan 14
-        if len(s) != len(t):
-            return False
-        countS, countT = {}, {}
-        for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0)
-            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return True
         
-        return countS == countT
