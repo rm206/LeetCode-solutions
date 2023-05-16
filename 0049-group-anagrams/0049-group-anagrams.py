98325b362f729f7f42d6d6b401f5ae0826ef212c
Rishabh <75585601+rm206@users.noreply.github.com>
2023-05-16 15:04:01 -0500

    Time: 116 ms (40.02%), Space: 20.5 MB (20.31%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,50 +1,13 @@
+import collections
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # Nov 27
-        # O(m * nlogn)
-#         builder = {}
-#         for curr in strs:
-#             temp = tuple(sorted(curr))
-#             if temp not in builder:
-#                 builder[temp] = [curr]
-#             else:
-#                 builder[temp].append(curr)
-        
-#         res = []
-#         for curr in builder:
-#             res.append(builder[curr])
-        
-#         return res
-        
-        # O(m * n)
-        # builder = {}
-        # for curr in strs:
-        #     temp = [0] * 26
-        #     for c in curr:
-        #         temp[ord(c) - ord('a')] += 1
-        #     temp = tuple(temp)
-            
-        #     if temp not in builder:
-        #         builder[temp] = [curr]
-        #     else:
-        #         builder[temp].append(curr)
-        
-        # res = []
-        # for curr in builder:
-        #     res.append(builder[curr])
-        
-        # return res
-
-        # Jan 9
-        commonDict = {}
-        for curr in strs:
-            sortedTupledCurr = tuple(sorted(curr))
-            if sortedTupledCurr not in commonDict:
-                commonDict[sortedTupledCurr] = [curr]
-            else:
-                commonDict[sortedTupledCurr].append(curr)
+        d = collections.defaultdict(list)
+        for s in strs:
+            d[tuple(sorted(s))].append(s)
         
         res = []
-        for curr in commonDict:
-            res.append(commonDict[curr])
+        for t in d:
+            res.append(d[t])
+        
         return res
