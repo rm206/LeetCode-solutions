f1ea30ceca38c6c12e7c02ad0b1eb62fb1c4b25d
Rishabh <75585601+rm206@users.noreply.github.com>
2023-05-20 15:06:28 -0500

    Time: 265 ms (12.81%), Space: 25.2 MB (13.20%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,57 +1,23 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        if len(nums) == 2:
-            return [nums[1], nums[0]]
-        pre = [1 for _ in range(len(nums))]
-        post = [1 for _ in range(len(nums))]
-        i = 1
-        while i < len(nums):
-            pre[i] = pre[i-1] * nums[i-1]
-            i += 1
-        i = len(nums) - 2
-        while i >= 0:
-            post[i] = post[i+1] * nums[i+1]
-            i -= 1
-        ans = [pre[i] * post[i] for i in range(len(nums))]
-        return ans
-
-        res = [1 for _ in range(len(nums))]
-        pre = 1
-        for i in range(len(nums)):
-            res[i] *= pre
-            pre *= nums[i]
-        post = 1
-        for i in range(len(nums)-1, -1, -1):
-            res[i] *= post
-            post *= nums[i]
-        return res
-
-
-'''
-        if len(nums) == 2:
-            return [nums[1], nums[0]]
-        pre = [1 for _ in range(len(nums))]
-        post = [1 for _ in range(len(nums))]
-        i = 1
-        while i < len(nums):
-            pre[i] = pre[i-1] * nums[i-1]
-            i += 1
-        i = len(nums) - 2
-        while i >= 0:
-            post[i] = post[i+1] * nums[i+1]
-            i -= 1
-        ans = [pre[i] * post[i] for i in range(len(nums))]
-        return ans
-
-        res = [1 for _ in range(len(nums))]
-        pre = 1
-        for i in range(len(nums)):
-            res[i] *= pre
-            pre *= nums[i]
-        post = 1
-        for i in range(len(nums)-1, -1, -1):
-            res[i] *= post
-            post *= nums[i]
-        return res
-'''
+        len_nums = len(nums)
+        pre = [1 for i in range(len_nums)]
+        post = [1 for i in range(len_nums)]
         
+        running_mult = 1
+        for i in range(0, len_nums):
+            pre[i] = running_mult
+            running_mult *= nums[i]
+        
+        running_mult = 1
+        for i in range(len_nums - 1, -1, -1):
+            post[i] = running_mult
+            running_mult *= nums[i]
+        
+        print(pre)
+        print(post)
+        res = []
+        for i in range(len_nums):
+            res.append(pre[i] * post[i])
+        
+        return res
