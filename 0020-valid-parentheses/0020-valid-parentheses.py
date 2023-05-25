eccf76189b731058a343019ebf36394812ae82b2
Rishabh <75585601+rm206@users.noreply.github.com>
2023-05-24 19:02:21 -0500

    Time: 43 ms (35.35%), Space: 16.4 MB (16.03%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,28 +1,22 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        if len(s) % 2 != 0:
-            return False
-
-        stack = [s[0]]
-
-        for i in s[1:]:
-            if i in "({[":
-                stack.append(i)
-            else:
-                if i == ')':
-                    if len(stack) != 0 and stack[-1] == '(':
-                        stack.pop()
-                    else:
-                        return False
-                if i == '}':
-                    if len(stack) != 0 and stack[-1] == '{':
-                        stack.pop()
-                    else:
-                        return False
-                if i == ']':
-                    if len(stack) != 0 and stack[-1] == '[':
-                        stack.pop()
-                    else:
-                        return False
-
+        stack = []
+        for c in s:
+            if c == '(' or c == '{' or c == '[':
+                stack.append(c)
+            if c == ')':
+                if len(stack) > 0 and stack[-1] == '(':
+                    stack.pop()
+                else:
+                    return False
+            if c == '}':
+                if len(stack) > 0 and stack[-1] == '{':
+                    stack.pop()
+                else:
+                    return False
+            if c == ']':
+                if len(stack) > 0 and stack[-1] == '[':
+                    stack.pop()
+                else:
+                    return False
         return len(stack) == 0
