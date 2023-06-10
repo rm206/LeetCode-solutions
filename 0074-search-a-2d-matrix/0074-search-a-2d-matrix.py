77367401ce1d8f3593b014815f311a05f144b8e3
Rishabh <75585601+rm206@users.noreply.github.com>
2023-06-10 16:47:57 -0500

    Time: 53 ms (73.03%), Space: 16.9 MB (48.37%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,35 +1,43 @@
 class Solution:
-    def find_row(self, matrix, rows, cols, target):
-        row_l, row_r = 0, rows - 1
-        while row_l <= row_r:
-            mid = (row_l + row_r)//2
-            if target >= matrix[mid][0] and target <= matrix[mid][cols-1]:
-                return mid
-            elif matrix[mid][0] > target:
-                row_r = mid - 1
-            else:
-                row_l = mid + 1
-        
-        return -1
-
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        ROWS, COLS = len(matrix), len(matrix[0])
-        if target < matrix[0][0] or target > matrix[ROWS-1][COLS-1]:
+        def findrow(m: int, n: int):
+            top, bottom = 0, m - 1
+            while top <= bottom:
+                mid = (top + bottom) // 2
+                
+                if matrix[mid][0] <= target and target <= matrix[mid][n - 1]:
+                    return mid
+                
+                elif target < matrix[mid][0]:
+                    bottom = mid - 1
+                
+                else:
+                    top = mid + 1
+            
+            return -1
+        
+        # find row to which the target belongs (use findrow)
+        # with the row, run the regular binary search
+        m, n = len(matrix), len(matrix[0])
+        target_row = findrow(m, n)
+        
+        if target_row == -1:
             return False
         
-        row = self.find_row(matrix, ROWS, COLS, target)
-        if row == -1:
-            return False
-        
-        col_l, col_r = 0, COLS - 1
-        while col_l <= col_r:
-            mid = (col_l + col_r)//2
-            if target == matrix[row][mid]:
+        l, r = 0, n - 1
+        while l <= r:
+            mid = (l + r) // 2
+            
+            if matrix[target_row][mid] == target:
                 return True
-            elif target < matrix[row][mid]:
-                col_r = mid - 1
+            
+            elif target < matrix[target_row][mid]:
+                r = mid - 1
+            
             else:
-                col_l = mid + 1
+                l = mid + 1
         
         return False
+        
+        
         
