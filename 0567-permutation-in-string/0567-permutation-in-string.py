34f207349720e1eb4f3841ad7b9a5b32158643fa
Rishabh <75585601+rm206@users.noreply.github.com>
2023-06-12 03:47:29 -0500

    Time: 89 ms (60.29%), Space: 16.5 MB (14.71%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,23 +1,31 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        l1 = len(s1)
-        l2 = len(s2)
+        l1, l2 = len(s1), len(s2)
+        
         if l1 > l2:
             return False
-        d1 = {}
+        
+        l, r = 0, 0
+        d1, d2 = {}, {}
+        
         for c in s1:
             d1[c] = 1 + d1.get(c, 0)
-        d2 = {}
-        l, r = 0, 0
-        for r in range(l2):
+        
+        while r < l2:
+            # fix window and dictionary it is longer than s1
             if r - l + 1 > l1:
                 if d2[s2[l]] > 1:
                     d2[s2[l]] -= 1
                 else:
                     d2.pop(s2[l])
                 l += 1
+            
             d2[s2[r]] = 1 + d2.get(s2[r], 0)
+            
+            # compare dicts with the curretn corrected window
             if d1 == d2:
                 return True
+            
+            r += 1
         
         return False
