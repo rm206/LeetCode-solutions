e2e1d20583adf280cef038385f791fca96087c70
Rishabh <75585601+rm206@users.noreply.github.com>
2023-06-12 01:56:19 -0500

    Time: 184 ms (25.39%), Space: 16.4 MB (28.11%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -6,17 +6,13 @@ class Solution:
             window_len = r - l + 1
             count[s[r]] = 1 + count.get(s[r], 0)
             
-            # check if window valid, if it is update res
-            if window_len - max(count.values()) <= k:
-                res = max(res, window_len)
-            
             # if window not valid, move left pointer till it is valid
-            else:
-                while window_len - max(count.values()) > k:
-                    count[s[l]] -= 1
-                    l += 1      
-                    window_len = r - l + 1
+            while window_len - max(count.values()) > k:
+                count[s[l]] -= 1
+                l += 1      
+                window_len = r - l + 1
             
+            res = max(res, window_len)
             r += 1
         
         return res
