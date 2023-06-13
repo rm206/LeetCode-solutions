e819b69c58f6bc506df8d4aa3654111d1c91f984
Rishabh <75585601+rm206@users.noreply.github.com>
2023-06-13 16:11:34 -0500

    Time: 107 ms (36.96%), Space: 26.1 MB (57.42%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,42 +8,64 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        def reverseList(head):
-            prev, curr = None, head
-
-            while curr:
-                next_temp = curr.next
-                curr.next = prev
-                prev = curr
-                curr = next_temp
-
-            return prev
-        
+                # find middle
         slow, fast = head, head.next
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
+
+        # reverse second half
+        second = slow.next
+        prev = slow.next = None
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+
+        # merge two halfs
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
+#         def reverseList(head):
+#             prev, curr = None, head
+
+#             while curr:
+#                 next_temp = curr.next
+#                 curr.next = prev
+#                 prev = curr
+#                 curr = next_temp
+
+#             return prev
         
-        r_head = slow.next
-        slow.next = None
-        r_head = reverseList(r_head)
+#         slow, fast = head, head.next
+#         while fast and fast.next:
+#             slow = slow.next
+#             fast = fast.next.next
         
-        dummy = ListNode()
-        curr = dummy
-        p1, p2 = head, r_head
-        turn = 1
+#         r_head = slow.next
+#         slow.next = None
+#         r_head = reverseList(r_head)
         
-        while p1 or p2:
-            if turn == 1:
-                curr.next = p1
-                curr = curr.next
-                p1 = p1.next
-                turn *= -1
-            else:
-                curr.next = p2
-                curr = curr.next
-                p2 = p2.next
-                turn *= -1
+#         dummy = ListNode()
+#         curr = dummy
+#         p1, p2 = head, r_head
+#         turn = 1
+        
+#         while p1 or p2:
+#             if turn == 1:
+#                 curr.next = p1
+#                 curr = curr.next
+#                 p1 = p1.next
+#                 turn *= -1
+#             else:
+#                 curr.next = p2
+#                 curr = curr.next
+#                 p2 = p2.next
+#                 turn *= -1
 
         
-        head = dummy.next
+#         head = dummy.next
