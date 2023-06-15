3df74f78defd70c6410e2ecbdc94c63ca2357723
Rishabh <75585601+rm206@users.noreply.github.com>
2023-06-15 00:15:11 -0500

    Time: 63 ms (36.12%), Space: 19.7 MB (5.60%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -5,29 +5,24 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isValidHelper(self, root: Optional[TreeNode], left, right):
-        if not root:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        res = []
+        
+        def helper(node):
+            if not node:
+                return
+            
+            helper(node.left)
+            res.append(node.val)
+            helper(node.right)
+        
+        helper(root)
+        print(res)
+        if len(res) < 2:
             return True
-        if not(root.val < right and root.val > left):
-            return False
-        return self.isValidHelper(root.left, left, root.val) and self.isValidHelper(root.right, root.val, right)
-            
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        return self.isValidHelper(root, float("-inf"), float("inf"))
-
-'''
-T: O(N) S: O(H+N)
-    def isValidHelper(self, root: Optional[TreeNode], arr):
-        if root:
-            self.isValidHelper(root.left, arr)
-            arr.append(root.val)
-            self.isValidHelper(root.right, arr)
-            
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        inOrder = []
-        self.isValidHelper(root, inOrder)
-        for i in range(len(inOrder) - 1):
-            if inOrder[i] >= inOrder[i+1]:
+        
+        for i in range(1, len(res)):
+            if res[i] <= res[i - 1]:
                 return False
+        
         return True
-'''
