dc46c741cbc3bac0707ad04f25a94384f4ad7232
Rishabh <75585601+rm206@users.noreply.github.com>
2023-06-23 01:27:31 -0500

    Time: 47 ms (70.5%), Space: 16.4 MB (30.45%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,43 +1,39 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        len_nums = len(nums)
-        if len_nums == 0:
-            return 0
-        
-        start, end = 0, len_nums - 1
-        while start <= end:
-            if nums[start] == val:
-                nums[start], nums[end] = nums[end], nums[start]
-                end -= 1
-            else:
-                start += 1
-        
-        return start
-
-'''
-        len_nums = len(nums)
-        if len_nums == 0:
-            return 0
-        
-        start, end = 0, len_nums - 1
-        while start <= end:
-            if nums[start] == val:
-                nums[start], nums[end] = nums[end], nums[start]
-                end -= 1
-            else:
-                start += 1
-        
-        return start
-
         i, index = 0, len(nums) - 1
-        while i < index:
+        while i <= index:
             if nums[i] == val:
                 nums[i], nums[index] = nums[index], nums[i]
                 index -= 1
-                while nums[i] == val:
-                    nums[i], nums[index] = nums[index], nums[i]
-                    index -= 1
-            i += 1
+            else:
+                i += 1
         
-        return i + 1
+        return i
+
+'''
+i, index = 0, len(nums) - 1
+while i < index:
+    if nums[i] == val:
+        nums[i], nums[index] = nums[index], nums[i]
+        index -= 1
+        while nums[i] == val:
+            nums[i], nums[index] = nums[index], nums[i]
+            index -= 1
+    i += 1
+
+return i + 1
+
+len_nums = len(nums)
+if len_nums == 0:
+    return 0
+
+start, end = 0, len_nums - 1
+while start <= end:
+    if nums[start] == val:
+        nums[start], nums[end] = nums[end], nums[start]
+        end -= 1
+    else:
+        start += 1
+
+return start
 '''
