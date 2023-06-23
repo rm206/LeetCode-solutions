3a1a403d0b91a387c05a3335f7d98bb04ac585a5
Rishabh <75585601+rm206@users.noreply.github.com>
2023-06-23 03:16:23 -0500

    Time: 55 ms (65.73%), Space: 16.5 MB (75.44%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -3,14 +3,30 @@ class Solution:
         forward_mapping = {}
         backward_mapping = {}
         for i in range(len(s)):
-            if s[i] not in forward_mapping:
-                if t[i] not in backward_mapping:
-                    forward_mapping[s[i]] = t[i]
-                    backward_mapping[t[i]] = s[i]
-                else:
-                    return False
-            else:
-                if t[i] != forward_mapping[s[i]]:
-                    return False
+            cs, ct = s[i], t[i]
+
+            if (cs in forward_mapping and forward_mapping[cs] != ct) or (ct in backward_mapping and backward_mapping[ct] != cs):
+                return False
+
+            forward_mapping[cs] = ct
+            backward_mapping[ct] = cs
         
         return True
+
+
+'''
+forward_mapping = {}
+backward_mapping = {}
+for i in range(len(s)):
+    if s[i] not in forward_mapping:
+        if t[i] not in backward_mapping:
+            forward_mapping[s[i]] = t[i]
+            backward_mapping[t[i]] = s[i]
+        else:
+            return False
+    else:
+        if t[i] != forward_mapping[s[i]]:
+            return False
+
+return True
+'''
