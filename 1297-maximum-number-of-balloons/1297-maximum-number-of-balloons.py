8c25bf27c3507b0790b1575ee7f62c8b7acae574
Rishabh <75585601+rm206@users.noreply.github.com>
2023-06-23 23:15:26 -0500

    Time: 55 ms (27.15%), Space: 16.5 MB (28.96%) - LeetHub

--- 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
@@ -8,7 +8,8 @@ class Solution:
                 m[t] = 1 + m.get(t, 0)
         
         if ('b' in m) and ('a' in m) and ('l' in m) and ('o' in m) and ('n' in m):
-            while True:
+            out_of_letters = False
+            while not out_of_letters:
                 if m['b'] >= 1 and m['a'] >= 1 and m['l'] >= 2 and m['o'] >= 2 and m['n'] >= 1:
                     res += 1
                     m['b'] -= 1
@@ -17,6 +18,6 @@ class Solution:
                     m['o'] -= 2
                     m['n'] -= 1
                 else:
-                    break
+                    out_of_letters = True
         
         return res
