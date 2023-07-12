ff9b30a3c915ebda89263cde32cda4ce9fe8f1a3
Rishabh <75585601+rm206@users.noreply.github.com>
2023-07-12 18:12:51 -0500

    Added solution - LeetHub

---
+++ Merge two binary Max heaps - GFG/merge-two-binary-max-heaps.py
@@ -0,0 +1,45 @@
+#User function Template for python3
+import heapq
+class Solution():
+    def mergeHeaps(self, a, b, n, m):
+        #your code here
+        merged = a + b
+        for i in range(n + m):
+            merged[i] *= -1
+        heapq.heapify(merged)
+        for i in range(n + m):
+            merged[i] *= -1
+        return merged
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+def isMerged(arr1, arr2, merged):
+    if (len(arr1) + len(arr2) != len(merged)):
+        return False
+    arr1 += arr2
+    arr1.sort()
+    mergedCopy = sorted(merged)
+    if (arr1 != mergedCopy):
+        return False
+    for i in range(1, len(merged)):
+        if merged[i] > merged[(i-1)//2]:
+            return False
+
+    return True
+
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        n, m = map(int, input().split())
+        a = [int(i) for i in input().split()]
+        b = [int(i) for i in input().split()]
+        copyA = a[:]
+        copyB = b[:]
+        obj = Solution()
+        merged = obj.mergeHeaps(a, b, n, m)
+        flag = isMerged(copyA, copyB, merged)
+        print(0 if flag == False else 1)
+
+# } Driver Code Ends
