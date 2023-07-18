5efea2022ae165952ae8a7e195618aaa3d993697
Rishabh <75585601+rm206@users.noreply.github.com>
2023-07-17 21:54:21 -0500

    Time: 43 ms (90.41%), Space: 16.5 MB (81.09%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,34 @@
+import copy
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        
+        # def solver(arr, incoming):
+        #     nonlocal res
+
+        #     templist = copy.copy(incoming)
+        #     res.append(templist)
+        #     for i in range(len(arr)):
+        #         incoming.append(arr[i])
+        #         solver(arr[i+1:], incoming)
+        #         incoming.pop()
+        
+        def solver(i):
+            nonlocal nums, subset, res
+
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+            
+            subset.append(nums[i])
+            solver(i + 1)
+
+            subset.pop()
+            solver(i + 1)
+            
+
+        subset = []
+        res = []
+        # nums.sort()
+        # solver(nums, [])
+        solver(0)
+        return res
