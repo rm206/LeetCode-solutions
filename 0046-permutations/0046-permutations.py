15650e5f82f322e6acecc0194f369bb6b4756d98
Rishabh <75585601+rm206@users.noreply.github.com>
2023-07-21 13:08:41 -0500

    Time: 50 ms (85.57%), Space: 16.7 MB (13.31%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,18 +1,21 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         
-        def solver(curr):
+        def solver(curr, curr_set):
             if len(curr) == len(nums):
                 res.append(curr.copy())
             
             for num in nums:
-                if num not in curr:
+                if num not in curr_set:
                     curr.append(num)
-                    solver(curr)
+                    curr_set.add(num)
+                    solver(curr, curr_set)
                     curr.pop()
+                    curr_set.remove(num)
         
         res = []
         curr = []
-        solver(curr)
+        curr_set = set()
+        solver(curr, curr_set)
         
         return res
