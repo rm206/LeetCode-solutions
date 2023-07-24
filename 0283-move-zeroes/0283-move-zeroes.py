fabaa5015e4540d779a8dc0d33db0668b0875715
Rishabh <75585601+rm206@users.noreply.github.com>
2023-07-23 21:21:15 -0500

    Time: 176 ms (68.39%), Space: 18 MB (10.35%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,9 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        last_zero_at = 0
+        last_non_zero_at = 0
         
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[last_zero_at], nums[i] = nums[i], nums[last_zero_at]
-                last_zero_at += 1
+                nums[last_non_zero_at], nums[i] = nums[i], nums[last_non_zero_at]
+                last_non_zero_at += 1
