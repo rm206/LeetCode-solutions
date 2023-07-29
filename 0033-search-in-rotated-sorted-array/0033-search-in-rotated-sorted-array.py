bf03cf11160d33b52df87709e9c228d96c6640f2
Rishabh <75585601+rm206@users.noreply.github.com>
2023-07-29 02:36:46 -0500

    Time: 47 ms (95.48%), Space: 16.7 MB (73.33%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,5 +1,44 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        def get_pivot(arr, n):
+	        l, r = 0, n - 1
+
+	        while l < r:
+		        mid = (l + r) // 2
+
+		        if arr[mid] >= arr[0]:
+			        l = mid + 1
+		        else:
+			        r = mid
+	
+	        return l
+
+        def bin_search(arr, l, r, target):
+	        while l <= r:
+		        mid = (l + r) // 2
+
+		        if arr[mid] == target:
+			        return mid
+		
+		        elif arr[mid] < target:
+			        l = mid + 1
+		
+		        else:
+			        r = mid - 1
+	
+	        return -1
+
+        n = len(nums)
+        k = target
+        pivot = get_pivot(nums, n)
+
+        if nums[pivot] <= k and k <= nums[n - 1]:
+            return bin_search(nums, pivot, n - 1, k)
+
+        else:
+            return bin_search(nums, 0, pivot - 1, k)
+
+'''
         l, r = 0, len(nums) - 1
         while l <= r:
             mid = (l + r) // 2
@@ -28,3 +67,4 @@ class Solution:
                     l = mid + 1
         
         return -1
+'''
