5c136de13c74041b78e60d6ca30f0c8532f16977
Rishabh <75585601+rm206@users.noreply.github.com>
2023-08-02 20:00:27 -0500

    Added solution - LeetHub

---
+++ Union of two arrays - GFG/union-of-two-arrays.py
@@ -0,0 +1,41 @@
+#User function Template for python3
+
+class Solution:    
+    #Function to return the count of number of elements in union of two arrays.
+    def doUnion(self,a,n,b,m):
+        
+        #code here
+        set_ab = set()
+        
+        res = 0
+        
+        for el in a:
+            if el not in set_ab:
+                set_ab.add(el)
+                res += 1
+
+        for el in b:
+            if el not in set_ab:
+                set_ab.add(el)
+                res += 1
+        
+        return res
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        
+        n,m=[int(x) for x in input().strip().split()]
+        
+        a=[int(x) for x in input().strip().split()]
+        b=[int(x) for x in input().strip().split()]
+        ob=Solution()
+        
+        print(ob.doUnion(a,n,b,m))
+# } Driver Code Ends
