4545988049901ecc090afa5930dfa81e5938307b
Rishabh <75585601+rm206@users.noreply.github.com>
2023-08-08 14:45:03 -0500

    Time: 139 ms (87.21%), Space: 17.7 MB (58.43%) - LeetHub

--- 0910-smallest-range-ii/0910-smallest-range-ii.py
+++ 0910-smallest-range-ii/0910-smallest-range-ii.py
@@ -1,11 +1,13 @@
 class Solution:
-    def smallestRangeII(self, nums: List[int], k: int) -> int:      
+    def smallestRangeII(self, nums: List[int], k: int) -> int:
+        n = len(nums)
         nums.sort()
-        small = nums[0] 
-        big = nums[-1] 
+        big = nums[-1]
+        small = nums[0]
+        
         diff = big - small
         
-        for i in range(len(nums) - 1):
+        for i in range(n - 1):
             mini = min(small + k, nums[i + 1] - k)
             maxi = max(big - k, nums[i] + k)
             
