6f2f2b3db605037ae3d95c043372030f36211a58
Rishabh <75585601+rm206@users.noreply.github.com>
2023-08-08 03:44:01 -0500

    Time: 139 ms (88.24%), Space: 17.6 MB (59.12%) - LeetHub

--- 0910-smallest-range-ii/0910-smallest-range-ii.py
+++ 0910-smallest-range-ii/0910-smallest-range-ii.py
@@ -1,31 +1,14 @@
 class Solution:
-    def smallestRangeII(self, nums: List[int], k: int) -> int:
+    def smallestRangeII(self, nums: List[int], k: int) -> int:      
         nums.sort()
-        mi, ma = nums[0], nums[-1]
-        ans = ma - mi
+        small = nums[0] 
+        big = nums[-1] 
+        diff = big - small
+        
         for i in range(len(nums) - 1):
-            a, b = nums[i], nums[i+1]
-            ans = min(ans, max(ma-k, a+k) - min(mi+k, b-k))
-        return ans
-        
-        
-'''
-        nums.sort()
-        n = len(nums)
-        # small = nums[0] + k
-        # big = nums[n - 1] - k
-        ma, mi = nums[0], nums[n - 1]
-        K = k
-        # diff = big - small
-        diff = ma - mi
-        
-        for i in range(n - 1):
-            # mini = min(small, nums[i + 1] - k)
-            # maxi = max(big, nums[i] + k)
+            mini = min(small + k, nums[i + 1] - k)
+            maxi = max(big - k, nums[i] + k)
             
-            # diff = min(diff, maxi - mini)
-            a, b = nums[i], nums[i+1]
-            diff = min(diff, max(ma-K, a+K) - min(mi+K, b-K))
+            diff = min(diff, maxi - mini)
         
         return diff
-'''
