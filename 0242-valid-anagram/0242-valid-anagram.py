a521551ae197e51589d2ffda6b4eed9b98894f2f
Rishabh <75585601+rm206@users.noreply.github.com>
2023-08-09 17:43:31 -0500

    Time: 62 ms (64.90%), Space: 16.7 MB (88.64%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,19 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        arrS = [0] * 26
-        arrT = [0] * 26
+        comp  = [0 for i in range(26)]
         
-        for c in s: # O(m)
-            arrS[ord(c) - ord('a')] += 1
-        for c in t: # O(n)
-            arrT[ord(c) - ord('a')] += 1
+        for c in s:
+            comp[ord(c) - ord('a')] += 1
         
-        for c in s: # O(m)
-            arrT[ord(c) - ord('a')] -= 1
+        for c in t:
+            comp[ord(c) - ord('a')] -= 1
         
-        for n in arrT: # O(26)
-            if n != 0:
+        for i in range(26):
+            if comp[i] != 0:
                 return False
         
         return True
-        
