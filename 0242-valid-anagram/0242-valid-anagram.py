46992c8c8e71ce12e7344927190177684b6216ed
Rishabh <75585601+rm206@users.noreply.github.com>
2023-08-09 17:40:18 -0500

    Time: 67 ms (48.85%), Space: 16.8 MB (88.64%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -3,15 +3,15 @@ class Solution:
         arrS = [0] * 26
         arrT = [0] * 26
         
-        for c in s:
+        for c in s: # O(m)
             arrS[ord(c) - ord('a')] += 1
-        for c in t:
+        for c in t: # O(n)
             arrT[ord(c) - ord('a')] += 1
         
-        for c in s:
+        for c in s: # O(m)
             arrT[ord(c) - ord('a')] -= 1
         
-        for n in arrT:
+        for n in arrT: # O(26)
             if n != 0:
                 return False
         
