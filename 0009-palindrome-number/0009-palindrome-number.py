7fe52818779de564d3d8b3dab67ea1738be31753
Rishabh <75585601+rm206@users.noreply.github.com>
2023-08-11 00:31:25 -0500

    Time: 59 ms (89.43%), Space: 16.1 MB (99.77%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -3,11 +3,25 @@ class Solution:
         if x < 0:
             return False
         
-        comp = []
+        rev = 0
+        n = x
         
-        while x:
-            val = x % 10
-            comp.append(val)
-            x = x // 10
+        while n:
+            rev = (rev * 10) + (n % 10)
+            n = n // 10
         
-        return comp == comp[::-1]
+        return rev == x
+        
+'''
+if x < 0:
+    return False
+
+comp = []
+
+while x:
+    val = x % 10
+    comp.append(val)
+    x = x // 10
+
+return comp == comp[::-1]
+'''
