93142ff0a9629fdb695b607bc789bb3f51a26b14
Rishabh <75585601+rm206@users.noreply.github.com>
2023-08-11 02:40:32 -0500

    Time: 109 ms (81.12%), Space: 17.7 MB (10.21%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -7,21 +7,42 @@
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         
+        def is_same_tree(s, t):
+            if not s and not t:
+                return True
+            
+            if (s and not t) or (not s and t):
+                return False
+            
+            return s.val == t.val and is_same_tree(s.left, t.left) and is_same_tree(s.right, t.right)
+        
         if not subRoot and root:
             return True
         
         elif subRoot and not root:
             return False
         
-        return self.isSameTree(root, subRoot) or self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot) 
-        
-    def isSameTree(self, s, t):
-        
-        if not s and not t:
-            return True
-        
-        elif (not s and t) or (s and not t):
-            return False
-        
-        else:
-            return s.val == t.val and self.isSameTree(s.left, t.left) and self.isSameTree(s.right, t.right)
+        return is_same_tree(root, subRoot) or self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+'''
+def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+
+    if not subRoot and root:
+        return True
+
+    elif subRoot and not root:
+        return False
+
+    return self.isSameTree(root, subRoot) or self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot) 
+
+def isSameTree(self, s, t):
+
+    if not s and not t:
+        return True
+
+    elif (not s and t) or (s and not t):
+        return False
+
+    else:
+        return s.val == t.val and self.isSameTree(s.left, t.left) and self.isSameTree(s.right, t.right)
+'''
