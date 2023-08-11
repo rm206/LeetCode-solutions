e2f313f2f2717eb85e656bdbda9b27d6d625fdef
Rishabh <75585601+rm206@users.noreply.github.com>
2023-08-11 01:59:29 -0500

    Time: 39 ms (88.96%), Space: 16.2 MB (61.59%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,20 +1,30 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        if n == 0:
-            return 0
-
-        builder = []
-
-        for i in range(32):
-            val = n % 2
-            n = n >> 1
-            builder.append(val)
-
-        res = 0
-        p = 31
         
-        for i in builder:
-            res += i * pow(2, p)
-            p -= 1
-
+        res = 0
+        for i in range(32):
+            bit = n >> i & 1
+            res += (bit << (31 - i))
         return res
+            
+        
+'''
+if n == 0:
+    return 0
+
+builder = []
+
+for i in range(32):
+    val = n % 2
+    n = n >> 1
+    builder.append(val)
+
+res = 0
+p = 31
+
+for i in builder:
+    res += i * pow(2, p)
+    p -= 1
+
+return res
+'''
