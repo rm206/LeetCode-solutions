1c8dafb0f5728f95eca95257e5ff1cec23c05602
Rishabh <75585601+rm206@users.noreply.github.com>
2023-08-14 00:32:01 -0500

    Time: 707 ms (68.42%), Space: 75 MB (5.76%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -1,34 +1,39 @@
-import collections
+from collections import defaultdict
+
 class TimeMap:
 
     def __init__(self):
-        self.store = collections.defaultdict(list)
+        self.d = defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        self.store[key].append([timestamp, value])
+        self.d[key].append([timestamp, value])
 
     def get(self, key: str, timestamp: int) -> str:
-        res = ""
-        vals = self.store.get(key, [])
-        
-        # find ans
-        l, r = 0, len(vals) - 1
-        while l <= r:
-            mid = (l + r) // 2
+        if key not in self.d:
+            return ""
+        else:
+            search_arr = self.d[key]
+            res = -1
+            l, r = 0, len(search_arr) - 1
             
-            if timestamp == vals[mid][0]:
-                return vals[mid][1]
+            while l <= r:
+                mid = (l + r) // 2
+                
+                if search_arr[mid][0] == timestamp:
+                    return search_arr[mid][1]
+                
+                elif search_arr[mid][0] < timestamp:
+                    res = mid
+                    l = mid + 1
+                
+                else:
+                    r = mid - 1
             
-            elif vals[mid][0] < timestamp:
-                res = vals[mid][1]
-                l = mid + 1
-            
+            if res == -1:
+                return ""
             else:
-                r = mid - 1
-    
-        return res
-        
-        
+                return search_arr[res][1]
+
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
