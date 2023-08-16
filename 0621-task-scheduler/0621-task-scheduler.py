02cb1fd41c788f142e04bfb6605de181766ba30e
Rishabh <75585601+rm206@users.noreply.github.com>
2023-08-15 19:45:47 -0500

    Time: 611 ms (44.65%), Space: 16.7 MB (95.91%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,25 +1,33 @@
-from collections import Counter, deque
 import heapq
+from collections import Counter
 
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        counts = Counter(tasks)
-        max_heap = [-c for c in counts.values()]
-
-        heapq.heapify(max_heap)
-
-        q = deque() # [count, time_when_available]
+        # place most occuring tasks first
+        # heap can maintain the curr most freq elt
+        # q can hold tasks in place
+        # global time to check if cooldown period ended
+        
+        max_heap = []
+        q = []
         time = 0
-
+        counts = Counter(tasks)
+        
+        for task, freq in counts.items():
+            heapq.heappush(max_heap, (-1 * freq, task))
+        
         while max_heap or q:
             time += 1
-
-            if max_heap:
-                new_cnt = 1 + heapq.heappop(max_heap)
-                if new_cnt != 0:
-                    q.append([new_cnt, time + n])
             
-            if q and q[0][1] == time:
-                heapq.heappush(max_heap, q.popleft()[0])
+            if q and q[0][2] == time:
+                el = q.pop(0)
+                heapq.heappush(max_heap, (-1 * el[0], el[1]))
+            
+            if max_heap:
+                elt = heapq.heappop(max_heap)
+                if (elt[0] * -1) - 1 > 0:
+                    q.append(((elt[0] * -1) - 1, elt[1], time + n + 1))
+            
+            
         
         return time
