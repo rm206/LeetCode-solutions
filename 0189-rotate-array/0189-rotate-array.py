c0ffd7481f581dfc7bf9ba92220c893e0c4d440c
Rishabh <75585601+rm206@users.noreply.github.com>
2023-08-17 01:10:45 -0500

    Time: 168 ms (98.48%), Space: 27.9 MB (36.78%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -6,29 +6,10 @@ class Solution:
         n = len(nums)
         k = k % n
         
-        # nums[: n - k] = nums[: n - k].reverse()
         l, r = 0, n - k - 1
-        while l <= r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l += 1
-            r -= 1
-            
-        # nums[k + 1 : ] = nums[k + 1 : ].reverse()
+        nums[l : r + 1] = nums[l : r + 1][::-1]
+        
         l, r = n - k, n - 1
-        while l <= r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l += 1
-            r -= 1
+        nums[l : r + 1] = nums[l : r + 1][::-1]
         
         nums.reverse()
-        
-        
-'''        
-temp = nums.copy()
-
-for i in range(len(nums)):
-    temp[(i + k) % len(nums)] = nums[i]
-
-for i in range(len(temp)):
-    nums[i] = temp[i]
-'''    
