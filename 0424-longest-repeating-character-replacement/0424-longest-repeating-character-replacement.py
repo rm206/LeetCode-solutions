68fa857cbf8f2cb1868a013d232b0f4d62e36d49
Rishabh <75585601+rm206@users.noreply.github.com>
2023-08-25 23:17:26 -0500

    Time: 148 ms (32.86%), Space: 16.5 MB (41.31%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,18 +1,21 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        l, r, res, count = 0, 0, 0, {}
+        curr = {}
+        res = 0
+        l, r = 0, 0
         
         while r < len(s):
-            window_len = r - l + 1
-            count[s[r]] = 1 + count.get(s[r], 0)
             
-            # if window not valid, move left pointer till it is valid
-            while window_len - max(count.values()) > k:
-                count[s[l]] -= 1
-                l += 1      
+            window_len = r - l + 1
+            curr[s[r]] = 1 + curr.get(s[r], 0)
+            
+            while window_len - max(curr.values()) > k:
+                curr[s[l]] -= 1
+                l += 1
                 window_len = r - l + 1
             
-            res = max(res, window_len)
+            res = max(res, r - l + 1)
+            
             r += 1
-        
+            
         return res
