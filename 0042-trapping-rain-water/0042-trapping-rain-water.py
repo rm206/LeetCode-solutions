2906da81aa1a257056859ece82c9dfc9d6640b75
Rishabh <75585601+rm206@users.noreply.github.com>
2023-09-05 00:48:04 -0500

    Time: 126 ms (49.15%), Space: 18.7 MB (29.31%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -5,20 +5,19 @@ class Solution:
         
         h = len(height)
         
-        left_max = [0 for i in range(h)]
+        maxes = [0 for i in range(h)]
         l = height[0]
-        for i in range(1, h):
-            left_max[i] = l
+        for i in range(1, h-1):
+            maxes[i] = l
             l = max(l, height[i])
         
-        right_max = [0 for i in range(h)]
         r = height[-1]
-        for i in range(h - 2, -1, -1):
-            right_max[i] = r
+        for i in range(h - 2, 0, -1):
+            maxes[i] = min(r, maxes[i])
             r = max(r, height[i])
-        
+            
         res = 0
-        for i in range(h):
-            res += max(min(left_max[i], right_max[i]) - height[i], 0)
+        for i in range(1, h-1):
+            res += max(maxes[i] - height[i], 0)
         
         return res
