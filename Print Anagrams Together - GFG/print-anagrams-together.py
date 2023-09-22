d498758e9e27953469c7fac9045452a7301af2a5
Rishabh <75585601+rm206@users.noreply.github.com>
2023-09-22 13:58:23 -0500

    Added solution - LeetHub

---
+++ Print Anagrams Together - GFG/print-anagrams-together.py
@@ -0,0 +1,43 @@
+#User function Template for python3
+from collections import defaultdict
+class Solution:
+    def Anagrams(self, words, n):
+        '''
+        words: list of word
+        n:      no of words
+        return : list of group of anagram {list will be sorted in driver code (not word in grp)}
+        '''
+        
+        #code here
+        d = defaultdict(list)
+        
+        for word in words:
+            d[tuple(sorted(word))].append(word)
+        
+        res = []
+        for key in d.keys():
+            res.append(d[key])
+        
+        return res
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__ =='__main__':
+    t= int(input())
+    for tcs in range(t):
+        n= int(input())
+        words=input().split()
+        
+        ob = Solution()
+        ans = ob.Anagrams(words,n)
+        
+        for grp in sorted(ans):
+            for word in grp:
+                print(word,end=' ')
+            print()
+
+# } Driver Code Ends
