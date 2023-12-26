8412adca0cfba4523dc098219f0c6ccd29d7b2a4
Rishabh <75585601+rm206@users.noreply.github.com>
2023-12-26 07:54:25 -0600

    Time: 89 ms (90.58%), Space: 21.4 MB (32.70%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,17 +1,13 @@
+from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        from collections import defaultdict
-        
-        build = defaultdict(list)
+        groups = defaultdict(list)
         
         for s in strs:
-            build[tuple(sorted(s))].append(s)
+            groups[tuple(sorted(s))].append(s)
         
         res = []
-        for key, val in build.items():
-            temp = []
-            for v in val:
-                temp.append(v)
-            res.append(temp)
+        for v in groups.values():
+            res.append(v)
         
         return res
