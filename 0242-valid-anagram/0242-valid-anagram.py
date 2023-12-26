07482b5dafa5fc3bdcb76bf821c6faa6b29158b2
Rishabh <75585601+rm206@users.noreply.github.com>
2023-12-26 07:30:41 -0600

    Time: 41 ms (94.91%), Space: 17.9 MB (6.26%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,34 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        comp = {}
+        al_s = [0 for i in range(26)]
+        al_t = [0 for i in range(26)]
         
         for c in s:
-            comp[c] = 1 + comp.get(c, 0)
-        
+            al_s[ord(c) - ord('a')] += 1
         for c in t:
-            if c not in comp:
-                return False
-            
-            comp[c] -= 1
-            if comp[c] <= 0:
-                comp.pop(c)
+            al_t[ord(c) - ord('a')] += 1
         
-        return comp == {}
-        
-        
-
-'''
-comp  = [0 for i in range(26)]
-
-for c in s:
-    comp[ord(c) - ord('a')] += 1
-
-for c in t:
-    comp[ord(c) - ord('a')] -= 1
-
-for i in range(26):
-    if comp[i] != 0:
-        return False
-
-return True
-'''
+        return al_s == al_t
