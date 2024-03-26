d8366c5e6aa0a18ee729dd54a2b6810118616706
Rishabh <75585601+rm206@users.noreply.github.com>
2024-03-26 01:29:38 -0500

    Time: 456 ms (57.58%), Space: 31 MB (70.61%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -4,14 +4,14 @@ class Solution:
         
         l, r = 0, n - 1
         
-        while l <= r:
+        while l < r:
             mid = (l + r) // 2
             
             if arr[mid-1] < arr[mid] and arr[mid] > arr[mid + 1]:
                 return mid
             
             elif arr[mid - 1] < arr[mid]:
-                l = mid
+                l = mid + 1
             
             else:
                 r = mid
