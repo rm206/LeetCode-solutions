afffbf1ba3a02834d8f60fe3e3fb9ad75e2e2d10
Rishabh <75585601+rm206@users.noreply.github.com>
2024-04-09 13:05:33 -0500

    Time: 45 ms (62.19%), Space: 16.8 MB (5.73%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -0,0 +1,10 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        time = 0
+        while True:
+            for i in range(len(tickets)):
+                if tickets[i] > 0:
+                    tickets[i] -= 1
+                    time += 1
+                if tickets[i] == 0 and i == k:
+                    return time
