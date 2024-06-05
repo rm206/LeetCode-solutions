e2bd010aaa54a5ad89cccf81c1f5326e61624f31
Rishabh <75585601+rm206@users.noreply.github.com>
2024-06-05 03:34:32 -0500

    Time: 38 ms (49.07%), Space: 16.6 MB (34.1%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -4,16 +4,19 @@ class Solution:
         for c in s:
             d[c] = 1 + d.get(c, 0)
         
+        odd_exists = 0
         res = 0
         for k, v in d.items():
             if v >= 2:
                 while d[k] > 1:
                     res += 2
                     d[k] -= 2
+                if d[k] == 1:
+                    odd_exists += 1
+            else:
+                odd_exists += 1
         
-        for k, v in d.items():
-            if v == 1:
-                res += 1
-                break
+        if odd_exists:
+            res += 1
         
         return res
