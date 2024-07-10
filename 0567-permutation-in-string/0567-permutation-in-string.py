5ac44fd2b8d4944b54c34d522d80a5eb604e3006
Rishabh <75585601+rm206@users.noreply.github.com>
2024-07-10 00:33:52 -0500

    Time: 55 ms (76.49%), Space: 16.7 MB (17.79%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,48 +1,21 @@
-from collections import Counter
-
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        l1, l2 = len(s1), len(s2)
-        
-        if l1 > l2:
-            return False
-        
-        l, r = 0, l1 - 1
-        d1 = Counter(s1)
-        
-        while r < l2:
-            if Counter(s2[l : r + 1]) == d1:
+        d1 = [0] * 26
+        for c in s1:
+            d1[ord(c) - ord('a')] += 1
+
+        d2 = [0] * 26
+        l, r = 0, 0
+
+        for r in range(len(s2)):
+            while r - l + 1 > len(s1):
+                d2[ord(s2[l]) - ord('a')] -= 1
+                l += 1
+            
+            d2[ord(s2[r]) - ord('a')] += 1
+
+            if d1 == d2:
                 return True
-            l += 1
-            r += 1
+            
         
         return False
-#         l1, l2 = len(s1), len(s2)
-        
-#         if l1 > l2:
-#             return False
-        
-#         l, r = 0, 0
-#         d1, d2 = {}, {}
-        
-#         for c in s1:
-#             d1[c] = 1 + d1.get(c, 0)
-        
-#         while r < l2:
-#             # fix window and dictionary it is longer than s1
-#             if r - l + 1 > l1:
-#                 if d2[s2[l]] > 1:
-#                     d2[s2[l]] -= 1
-#                 else:
-#                     d2.pop(s2[l])
-#                 l += 1
-            
-#             d2[s2[r]] = 1 + d2.get(s2[r], 0)
-            
-#             # compare dicts with the current corrected window
-#             if d1 == d2:
-#                 return True
-            
-#             r += 1
-        
-#         return False
