42ca5bec95f6ec1e84d73f23d82897e2c3699eff
Rishabh <75585601+rm206@users.noreply.github.com>
2024-07-11 17:48:48 -0500

    Time: 36 ms (50.59%), Space: 16.6 MB (6.19%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,34 +1,27 @@
-import copy
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if digits == "":
             return []
-        
-        mapp = {'2' : "abc",
-               '3' : "def",
-               '4' : "ghi",
-               '5' : "jkl",
-               '6' : "mno",
-               '7' : "pqrs",
-               '8' : "tuv",
-               '9' : "wxyz"}
-        
-        def solver(i, curr):
-            # base case
-            if i == len(digits):
-                temp = copy.copy(curr)
-                res.append("".join(temp))
-                return
             
-            s = mapp[digits[i]]
-            for l in range(len(s)):
-                curr.append(s[l]) 
-                solver(i + 1, curr)
-                curr.pop()
+        keyboard = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+
+        def solver(curr, curr_len):
+            if curr_len == len(digits):
+                res.append(curr)
+                return
+
+            for c in keyboard[digits[curr_len]]:
+                solver(curr+c, curr_len+1)
         
         res = []
-        curr = []
-        i = 0
-        solver(i, curr)
-        
+        solver("", 0)
         return res
