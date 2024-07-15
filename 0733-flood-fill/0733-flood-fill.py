ed10a21b738b1ce617a09db172aaadcab5d45f26
Rishabh <75585601+rm206@users.noreply.github.com>
2024-07-15 18:10:33 -0500

    Time: 69 ms (41.89%), Space: 16.7 MB (31.81%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,19 +1,23 @@
 class Solution:
-    def floodFillHelper(self, image, sr, sc, new_color, original_color):
-        if (sr < 0) or (sr > len(image) - 1) or (sc < 0) or (sc > len(image[0]) - 1) or (image[sr][sc] != original_color):
-            return
-        
-        image[sr][sc] = new_color
-        self.floodFillHelper(image, sr-1, sc, new_color, original_color)
-        self.floodFillHelper(image, sr+1, sc, new_color, original_color)
-        self.floodFillHelper(image, sr, sc-1, new_color, original_color)
-        self.floodFillHelper(image, sr, sc+1, new_color, original_color)
-        
-        
-    
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        if image[sr][sc] == color:
-            return image
+        def solver(sr, sc, color, parent):
+            if sr < 0 or sr >= m or sc < 0 or sc >= n:
+                return
+            if image[sr][sc] == color:
+                return
+            
+            if image[sr][sc] != parent:
+                return
+            
+            image[sr][sc] = color
+            
+            solver(sr - 1, sc, color, parent)
+            solver(sr + 1, sc, color, parent)
+            solver(sr, sc - 1, color, parent)
+            solver(sr, sc + 1, color, parent)
         
-        self.floodFillHelper(image, sr, sc, color, image[sr][sc])
+        
+        m, n = len(image), len(image[0])
+        parent = image[sr][sc]
+        solver(sr, sc, color, parent)
         return image
