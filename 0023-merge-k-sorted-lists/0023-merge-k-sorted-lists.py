0ec37aceb57498d7f634068ea243008cfe347a5e
Rishabh <75585601+rm206@users.noreply.github.com>
2024-07-21 23:04:58 -0500

    Time: 75 ms (72.27%), Space: 20 MB (12.01%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -3,33 +3,25 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+import heapq
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        
-        def merge_2_lists(p1, p2):
-            dummy = ListNode(0, None)
-            curr = dummy
-            while p1 or p2:
-                val1 = p1.val if p1 else float('inf')
-                val2 = p2.val if p2 else float('inf')
+        heap = []
+        dummy = ListNode(-1, None)
+        curr = dummy
 
-                if val1 < val2:
-                    curr.next = p1
-                    p1 = p1.next
-                else:
-                    curr.next = p2
-                    p2 = p2.next
-
-                curr = curr.next
-
-            return dummy.next
+        cnt = 0
+        for l in lists:
+            if l:
+                heapq.heappush(heap, [l.val, cnt, l])
+                cnt += 1
         
-        if not lists:
-            return None
-        if len(lists) == 0:
-            return lists[0]
+        while heap:
+            val, c, node = heapq.heappop(heap)
+            curr.next = node
+            curr = curr.next
+            if node.next:
+                heapq.heappush(heap, [node.next.val, cnt, node.next])
+                cnt += 1
         
-        for i in range(1, len(lists)):
-            lists[i] = merge_2_lists(lists[i - 1], lists[i])
-        
-        return lists[len(lists) - 1]
+        return dummy.next
