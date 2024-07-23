46cbf5597c505d42446baf8866321246b519a67b
Rishabh <75585601+rm206@users.noreply.github.com>
2024-07-22 19:21:44 -0500

    Time: 53 ms (35.92%), Space: 16.6 MB (37.68%) - LeetHub

---
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
@@ -0,0 +1,35 @@
+import heapq
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        c = {}
+
+        for n in nums:
+            c[n] = 1 + c.get(n, 0)
+        
+        temp = []
+        for k, v in c.items():
+            temp.append([v, k])
+        
+        temp.sort(key=lambda x: (x[0], -x[1]))
+        j = 0
+        i = 0
+
+        while j < len(temp):
+            freq, val = temp[j]            
+            for _ in range(freq):
+                nums[i] = val
+                i += 1
+            j += 1
+        
+        # heap = []
+        # for k, v in c.items():
+        #     heapq.heappush(heap, [v, k])
+        
+        # i = 0
+        # while heap:
+        #     iters, val = heapq.heappop(heap)
+        #     for _ in range(iters):
+        #         nums[i] = val
+        #         i += 1
+        
+        return nums
