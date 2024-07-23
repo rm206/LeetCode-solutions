77d68be38699ec975f2840dadb89ecb601b04b9f
Rishabh <75585601+rm206@users.noreply.github.com>
2024-07-22 20:16:29 -0500

    Time: 32 ms (99.87%), Space: 16.6 MB (37.68%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
@@ -6,30 +6,31 @@ class Solution:
         for n in nums:
             c[n] = 1 + c.get(n, 0)
         
-        temp = []
-        for k, v in c.items():
-            temp.append([v, k])
-        
-        temp.sort(key=lambda x: (x[0], -x[1]))
-        j = 0
-        i = 0
-
-        while j < len(temp):
-            freq, val = temp[j]            
-            for _ in range(freq):
-                nums[i] = val
-                i += 1
-            j += 1
-        
-        # heap = []
+        # temp = []
         # for k, v in c.items():
-        #     heapq.heappush(heap, [v, k])
+        #     temp.append([v, k])
         
+        # temp.sort(key=lambda x: (x[0], -x[1]))
+        # j = 0
         # i = 0
-        # while heap:
-        #     iters, val = heapq.heappop(heap)
-        #     for _ in range(iters):
+
+        # while j < len(temp):
+        #     freq, val = temp[j]            
+        #     for _ in range(freq):
         #         nums[i] = val
         #         i += 1
+        #     j += 1
+        
+        heap = []
+        for k, v in c.items():
+            heapq.heappush(heap, [v, -k])
+        
+        i = 0
+        while heap:
+            iters, val = heapq.heappop(heap)
+            val *= -1
+            for _ in range(iters):
+                nums[i] = val
+                i += 1
         
         return nums
