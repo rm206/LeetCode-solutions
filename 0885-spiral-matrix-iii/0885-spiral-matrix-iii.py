98f716df5da50371045e8474300fd828947ee85d
Rishabh <75585601+rm206@users.noreply.github.com>
2024-08-08 04:32:04 -0500

    Time: 82 ms (61.72%), Space: 17.9 MB (38.67%) - LeetHub

--- 0885-spiral-matrix-iii/0885-spiral-matrix-iii.py
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.py
@@ -3,38 +3,53 @@ class Solution:
         def is_safe(r, c):
             return r >= 0 and r < rows and c >= 0 and c < cols
         
+        dirs = [[0,1],[1,0],[0,-1],[-1,0]] # right, down, left, up
         res = []
         res_len = 0
         ctr = 1
         rcurr, ccurr = rStart, cStart
+        i = 0
 
         while res_len != rows*cols:
-            for _ in range(ctr):
-                if is_safe(rcurr, ccurr):
-                    res.append([rcurr, ccurr])
-                    res_len += 1
-                ccurr += 1
-            
-            for _ in range(ctr):
-                if is_safe(rcurr, ccurr):
-                    res.append([rcurr, ccurr])
-                    res_len += 1
-                rcurr += 1
-            
-            ctr += 1
-
-            for _ in range(ctr):
-                if is_safe(rcurr, ccurr):
-                    res.append([rcurr, ccurr])
-                    res_len += 1
-                ccurr -= 1
-            
-            for _ in range(ctr):
-                if is_safe(rcurr, ccurr):
-                    res.append([rcurr, ccurr])
-                    res_len += 1
-                rcurr -= 1
+            for dont_matter in range(2):
+                dr, dc = dirs[i]
+                for _ in range(ctr):
+                    if (0 <= rcurr < rows and 0 <= ccurr < cols):
+                        res.append([rcurr, ccurr])
+                        res_len += 1
+                    rcurr, ccurr = rcurr + dr, ccurr + dc
+                i = (i + 1) % 4
             
             ctr += 1
 
         return res
+
+"""
+for _ in range(ctr):
+    if is_safe(rcurr, ccurr):
+        res.append([rcurr, ccurr])
+        res_len += 1
+    ccurr += 1
+
+for _ in range(ctr):
+    if is_safe(rcurr, ccurr):
+        res.append([rcurr, ccurr])
+        res_len += 1
+    rcurr += 1
+
+ctr += 1
+
+for _ in range(ctr):
+    if is_safe(rcurr, ccurr):
+        res.append([rcurr, ccurr])
+        res_len += 1
+    ccurr -= 1
+
+for _ in range(ctr):
+    if is_safe(rcurr, ccurr):
+        res.append([rcurr, ccurr])
+        res_len += 1
+    rcurr -= 1
+
+ctr += 1
+"""
