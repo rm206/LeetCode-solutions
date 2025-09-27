e947d60ce8802aaa2af1be219f698d25f1e9c94a
Rishabh <75585601+rm206@users.noreply.github.com>
2025-09-26 23:34:54 -0500

    Time: 12 ms (52.95%), Space: 17.9 MB (64.53%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,11 +1,22 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        al_s = [0 for i in range(26)]
-        al_t = [0 for i in range(26)]
+        # what if strings empty
+        # what if same
+        # lower case english only?
+
+        if len(s) != len(t):
+            return False
         
+        chars = [0] * 26
+
         for c in s:
-            al_s[ord(c) - ord('a')] += 1
-        for c in t:
-            al_t[ord(c) - ord('a')] += 1
+            chars[ord(c)-ord('a')] += 1
         
-        return al_s == al_t
+        for c in t:
+            chars[ord(c)-ord('a')] -= 1
+        
+        for n in chars:
+            if n != 0:
+                return False
+        
+        return True
