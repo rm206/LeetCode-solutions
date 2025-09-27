ba01a912d5d3b2bd9c0f3200b55a943ba3193382
Rishabh <75585601+rm206@users.noreply.github.com>
2025-09-27 14:43:50 -0500

    Time: 27 ms (46.49%), Space: 23.4 MB (41.35%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,36 +1,17 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [None] * len(nums)
 
-        res = [None for _ in range(len(nums))]
-        
-        l_mult = 1
+        curr = 1
+
         for i in range(len(nums)):
-            res[i] = l_mult
-            l_mult *= nums[i]
+            res[i] = curr
+            curr = curr * nums[i]
         
-        r_mult = 1
-        for i in range(len(nums) - 1, -1, -1):
-            res[i] = res[i] * r_mult
-            r_mult *= nums[i]
+        curr = 1
+
+        for i in range(len(nums)-1, -1, -1):
+            res[i] = res[i] * curr
+            curr = curr * nums[i]
         
         return res
-        
-'''
-l_mult = 1
-left = []
-for i in range(len(nums)):
-    left.append(l_mult)
-    l_mult *= nums[i]
-
-r_mult = 1
-right = [0 for _ in range(len(nums))]
-for i in range(len(nums) - 1, -1, -1):
-    right[i] = r_mult
-    r_mult *= nums[i]
-
-res = []
-for i in range(len(nums)):
-    res.append(left[i] * right[i])
-
-return res
-'''
