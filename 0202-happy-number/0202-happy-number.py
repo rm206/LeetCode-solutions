7acfef0508eb5feab83664649bac4848d313c354
Rishabh <75585601+rm206@users.noreply.github.com>
2026-02-28 23:58:35 -0600

    Time: 3 ms (37.66%), Space: 19.3 MB (81.7%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,6 +1,5 @@
-class Solution:
-    def isHappy(self, n: int) -> bool:
-        seen = set()
+"""
+seen = set()
         while True:
             s = sum(list(map(lambda x: int(x)**2, list(str(n)))))
             if s == 1:
@@ -9,3 +8,17 @@ class Solution:
                 return False
             seen.add(s)
             n = s
+"""
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def sum_square_digit(num):
+            return sum(list(map(lambda x: int(x)**2, list(str(num)))))
+        
+        slow, fast = n, n
+        while True:
+            slow = sum_square_digit(slow)
+            fast = sum_square_digit(sum_square_digit(fast))
+            if slow == fast:
+                break
+        
+        return slow == 1
