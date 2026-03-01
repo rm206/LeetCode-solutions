20a5cb4e1022f3e13d7dfa0de44ed06a2675e7a9
Rishabh <75585601+rm206@users.noreply.github.com>
2026-02-28 21:37:15 -0600

    Time: 0 ms (100%), Space: 20.2 MB (77.07%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
@@ -12,16 +12,17 @@ class Solution:
         q.append((root, 1))
 
         while q:
-            level = []
             l = len(q)
-            for _ in range(l):
+            _, first_index = q[0]
+            for i in range(l):
                 node, index = q.pop(0)
-                level.append((node, index))
+
+                if i == l-1:
+                    res = max(res, index-first_index+1)
+                
                 if node.left:
-                    q.append((node.left, 2*index))
+                    q.append((node.left, 2*(index - first_index)))
                 if node.right:
-                    q.append((node.right, 2*index+1))
-            width = level[-1][1]-level[0][1]+1
-            res = max(res, width)
+                    q.append((node.right, 2*(index - first_index)+1))
         
         return res
