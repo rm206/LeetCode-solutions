442199895f8f8a59e6d48e98c046a23ab0d153d6
Rishabh <75585601+rm206@users.noreply.github.com>
2026-03-05 23:55:41 -0600

    Time: 0 ms (100%), Space: 19.4 MB (38.66%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,23 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        t = []
-        for i in range(m):
-            t.append(nums1[i])
-            nums1[i] = 0
-        
-        p1, p2 = 0, 0
-        p = 0
-        
-        while p < m + n:
-            val1 = t[p1] if p1 < m else float('inf')
-            val2 = nums2[p2] if p2 < n else float('inf')
-            
-            if val1 < val2:
-                nums1[p] = val1
-                p1 += 1
+        builder_ptr = m+n-1
+        p1, p2 = m-1, n-1
+
+        while p1 > -1 or p2 > -1:
+            val1 = nums1[p1] if p1 >= 0 else -math.inf
+            val2 = nums2[p2] if p2 >= 0 else -math.inf
+
+            if val1 >= val2:
+                nums1[builder_ptr] = val1
+                p1 -=1
+                
             else:
-                nums1[p] = val2
-                p2 += 1
+                nums1[builder_ptr] = val2
+                p2 -=1
             
-            p += 1
+            builder_ptr -= 1
