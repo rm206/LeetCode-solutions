9cd8479ed18f1fd6bebaab58b691d44580185b6e
Rishabh <75585601+rm206@users.noreply.github.com>
2026-03-06 20:09:01 -0600

    Time: 35 ms (52.54%), Space: 22.7 MB (53.97%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
@@ -1,18 +1,16 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        def atmost(target):
-            if target < 0:
-                return 0
-            l, r = 0, 0
-            run_sum = 0
-            cnt = 0
-            for r in range(len(nums)):
-                run_sum += nums[r]
-                while run_sum > target:
-                    run_sum -= nums[l]
-                    l += 1
-                if run_sum <= goal:
-                    cnt += (r - l + 1)
-            return cnt
+        k = goal
+        mp = defaultdict(int)
+        mp[0] = 1
+        run_sum = 0
+        res = 0
+        for n in nums:
+            run_sum += n
+
+            if run_sum-k in mp:
+                res += mp[run_sum-k]
+            
+            mp[run_sum] += 1
         
-        return atmost(goal) - atmost(goal - 1)
+        return res
