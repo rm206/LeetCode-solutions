f650c400239cb380bff6f3b96dbd096f8c414aba
Rishabh <75585601+rm206@users.noreply.github.com>
2026-03-07 02:16:21 -0600

    Time: 0 ms (100%), Space: 26.9 MB (85.31%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,5 +1,9 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
+
+        def is_not_paired(index):
+            return(index > 0 and index < n-1 and nums[index-1] != nums[index] and nums[index] != nums[index+1]) or (index == 0 and nums[index] != nums[index+1]) or (index == n-1 and nums[index] != nums[index-1])
+
         n = len(nums)
 
         if n == 1:
@@ -10,7 +14,7 @@ class Solution:
         while l <= r:
             m = (l + r) // 2
 
-            if (m > 0 and m<n-1 and nums[m-1] != nums[m] and nums[m] != nums[m+1]) or (m == 0 and nums[m] != nums[m+1]) or (m == n-1 and nums[m] != nums[m-1]):
+            if is_not_paired(m):
                 return nums[m]
             
             elif (m > 0 and nums[m-1] == nums[m]) :
