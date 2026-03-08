97134e14646e37e88e5e9adca19963a57988706f
Rishabh <75585601+rm206@users.noreply.github.com>
2026-03-08 18:10:19 -0500

    Time: 7 ms (70.82%), Space: 23 MB (43.04%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -4,16 +4,11 @@ class Solution:
         stack = []
 
         for new_s, new_e in intervals:
-            to_append_1, to_append_2 = None, None
+            to_append_1, to_append_2 = new_s, new_e
 
-            if not stack:
-                to_append_1, to_append_2 = new_s, new_e
-            else:
-                if stack[-1][0] <= new_s <= stack[-1][1]:
-                    pop_s, pop_e = stack.pop()
-                    to_append_1, to_append_2 = min(pop_s, new_s), max(pop_e, new_e)
-                else:
-                    to_append_1, to_append_2 = new_s, new_e
+            if stack and stack[-1][0] <= new_s <= stack[-1][1]:
+                pop_s, pop_e = stack.pop()
+                to_append_1, to_append_2 = min(pop_s, new_s), max(pop_e, new_e)
             
             stack.append([to_append_1, to_append_2])
         
