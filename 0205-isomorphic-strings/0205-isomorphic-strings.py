c10ef94f6eda41ef9e5bf25a4d8788db84efce6d
Rishabh <75585601+rm206@users.noreply.github.com>
2026-03-08 22:23:45 -0500

    Time: 7 ms (38.53%), Space: 19.2 MB (96.07%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,32 +1,27 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        forward_mapping = {}
-        backward_mapping = {}
+        st = {}
+        ts = {}
+
         for i in range(len(s)):
-            cs, ct = s[i], t[i]
+            char_s = s[i]
+            char_t = t[i]
 
-            if (cs in forward_mapping and forward_mapping[cs] != ct) or (ct in backward_mapping and backward_mapping[ct] != cs):
+            if char_s in st and st[char_s] != char_t:
                 return False
-
-            forward_mapping[cs] = ct
-            backward_mapping[ct] = cs
+            
+            if char_t in ts and ts[char_t] != char_s:
+                return False
+            
+            st[char_s] = char_t
+            ts[char_t] = char_s
         
         return True
 
-
-'''
-forward_mapping = {}
-backward_mapping = {}
-for i in range(len(s)):
-    if s[i] not in forward_mapping:
-        if t[i] not in backward_mapping:
-            forward_mapping[s[i]] = t[i]
-            backward_mapping[t[i]] = s[i]
-        else:
-            return False
-    else:
-        if t[i] != forward_mapping[s[i]]:
-            return False
-
-return True
-'''
+        # e g g
+        # a d b
+        """
+        e a | a e
+        g d | d g
+            | b g
+        """
