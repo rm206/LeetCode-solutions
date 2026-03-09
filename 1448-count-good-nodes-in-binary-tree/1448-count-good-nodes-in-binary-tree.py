3116e7074cd2c4c722c0cca143c3091106eee2e3
Rishabh <75585601+rm206@users.noreply.github.com>
2026-03-09 12:51:24 -0500

    Time: 133 ms (40.9%), Space: 31.9 MB (89.1%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -6,19 +6,17 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        
-        def helper(node, curr_max):
+        def solver(node, max_so_far):
+            nonlocal res
             if not node:
-                return 0
+                return
             
-            res = 1 if node.val >= curr_max else 0
-            curr_max = max(curr_max, node.val)
+            if node.val >= max_so_far:
+                res += 1
             
-            res += helper(node.left, curr_max)
-            res += helper(node.right, curr_max)
-            
-            return res
-        
-        return helper(root, root.val)
-        
-        
+            solver(node.left, max(max_so_far, node.val))
+            solver(node.right, max(max_so_far, node.val))
+
+        res = 0
+        solver(root, -math.inf)
+        return res
