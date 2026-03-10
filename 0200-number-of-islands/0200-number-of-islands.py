302fd8e586e26093aec10beb88c6a535e59c2f35
Rishabh <75585601+rm206@users.noreply.github.com>
2026-03-10 04:18:58 -0500

    Time: 251 ms (43.58%), Space: 31.2 MB (9.34%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,22 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def is_safe(row, col):
-            return row >= 0 and col >= 0 and row < len(grid) and col < len(grid[0]) and grid[row][col] == "1" and visited[row][col] == False
+        ROWS, COLS = len(grid), len(grid[0])
+
+        def dfs(i, j):
+            if i < 0 or i >= ROWS or j < 0 or j >= COLS or grid[i][j] != "1":
+                return
+            visited.add((i, j))
+            if (i+1, j) not in visited:
+                dfs(i+1, j)
+            if (i-1, j) not in visited:
+                dfs(i-1, j)
+            if (i, j+1) not in visited:
+                dfs(i, j+1)
+            if (i, j-1) not in visited:
+                dfs(i, j-1)
         
-        def recurse(row, col):
-            nonlocal visited, dirs
-            visited[row][col] = True
-            
-            for dx, dy in dirs:
-                if is_safe(row+dx, col+dy):
-                    recurse(row+dx, col+dy)
-        
-        dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-        visited = [[False for i in range(len(grid[0]))] for j in range(len(grid))]
-        res = 0
+        ctr = 0
+        visited = set()
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == "1" and visited[i][j] == False:
-                    res += 1
-                    recurse(i, j)
-        return res
+                if (i, j) not in visited and grid[i][j] == "1":
+                    ctr += 1
+                    dfs(i, j)
+        return ctr
