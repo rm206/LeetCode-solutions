586d54af66a2757ca00365ad5621c5058d51cf0c
Rishabh <75585601+rm206@users.noreply.github.com>
2026-03-10 04:29:48 -0500

    Time: 277 ms (14.64%), Space: 31.2 MB (9.34%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,7 +3,10 @@ class Solution:
         ROWS, COLS = len(grid), len(grid[0])
 
         def is_safe(row, col):
-            return row >= 0 and row < ROWS and col >= 0 and col < COLS and grid[row][col] == "1" and (row, col) not in visited
+            return (row >= 0 and row < ROWS and 
+            col >= 0 and col < COLS and 
+            grid[row][col] == "1" and 
+            (row, col) not in visited)
 
         def dfs(i, j):
             visited.add((i, j))
@@ -14,8 +17,8 @@ class Solution:
         dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
         ctr = 0
         visited = set()
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        for i in range(ROWS):
+            for j in range(COLS):
                 if is_safe(i, j):
                     ctr += 1
                     dfs(i, j)
