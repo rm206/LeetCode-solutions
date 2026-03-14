23a7872dd84d1276a16ae32a19077952a0fc2421
Rishabh <75585601+rm206@users.noreply.github.com>
2026-03-14 16:18:02 -0500

    Time: 0 ms (100%), Space: 19.5 MB (60.56%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,16 +1,13 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        def solver(i, curr):
-            if i == len(nums):
+        def dfs(index, curr):
+            if index == len(nums):
                 res.append(curr.copy())
                 return
             
-            curr.append(nums[i])
-            solver(i + 1, curr)
-            curr.pop()
-            solver(i +1, curr)        
+            dfs(index+1, curr)
+            dfs(index+1, curr+[nums[index]])
         
         res = []
-        curr = []
-        solver(0, curr)
+        dfs(0, [])
         return res
