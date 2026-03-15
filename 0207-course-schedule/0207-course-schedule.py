d66b2e8c3f7708b3eda6b59060331be5dfad1e8d
Rishabh <75585601+rm206@users.noreply.github.com>
2026-03-15 14:06:02 -0500

    Time: 4 ms (66.49%), Space: 20.4 MB (77.8%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,11 +1,4 @@
-class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        def build_graph():
-            g = {x:[] for x in range(numCourses)}
-            for u, v in prerequisites:
-                g[u].append(v)
-            return g
-        
+"""
         def has_cycle(node):
             visited.add(node)
             dfs_visited.add(node)
@@ -29,3 +22,36 @@ class Solution:
                 return False
         
         return True
+        """
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        def build_graph():
+            g = {x:[] for x in range(numCourses)}
+            for u, v in prerequisites:
+                g[u].append(v)
+            return g
+        def indegrees():
+            i = {x:0 for x in range(numCourses)}
+            for node in graph:
+                for neigh in graph[node]:
+                    i[neigh] += 1
+            return i
+        
+        graph = build_graph()
+        indeg = indegrees()
+        q = []
+        for node in indeg:
+            if indeg[node] == 0:
+                q.append(node)
+        
+        res = []
+
+        while q:
+            node = q.pop(0)
+            res.append(node)
+            for neigh in graph[node]:
+                indeg[neigh] -= 1
+                if indeg[neigh] == 0:
+                    q.append(neigh)
+        
+        return len(res) == numCourses
