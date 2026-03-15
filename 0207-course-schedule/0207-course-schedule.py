5bcd6c2736bef96d2158174f77eb26f05b7fe3f4
Rishabh <75585601+rm206@users.noreply.github.com>
2026-03-15 13:39:57 -0500

    Time: 7 ms (46.22%), Space: 21.9 MB (6.21%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,28 +1,31 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = {x:[] for x in range(numCourses)}
-        for u, v in prerequisites:
-            graph[u].append(v)
+        def build_graph():
+            g = {x:[] for x in range(numCourses)}
+            for u, v in prerequisites:
+                g[u].append(v)
+            return g
         
-
         def has_cycle(node):
             visited.add(node)
             dfs_visited.add(node)
 
             for neigh in graph[node]:
-                if neigh in visited and neigh in dfs_visited:
-                    return True
-                if neigh in visited and neigh not in dfs_visited:
-                    pass
                 if neigh not in visited and has_cycle(neigh):
+                    return True
+                elif neigh in visited and neigh not in dfs_visited:
+                    pass
+                elif neigh in visited and neigh in dfs_visited:
                     return True
             
             dfs_visited.remove(node)
             return False
 
+        graph = build_graph()
         visited = set()
         dfs_visited = set()
-        for i in range(numCourses):
-            if i not in visited and has_cycle(i):
+        for n in range(numCourses):
+            if n not in visited and has_cycle(n):
                 return False
+        
         return True
