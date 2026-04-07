9259439e97fee2442b7665c12ea7fc81ef763fcb
Rishabh <75585601+rm206@users.noreply.github.com>
2026-04-06 23:16:57 -0500

    Time: 3 ms (75.71%), Space: 19.3 MB (55.77%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -2,37 +2,29 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
 
-        def is_safe(i, j):
-            return (
-                i >= 0 and
-                i < ROWS and
-                j >= 0 and
-                j < COLS and
-                grid[i][j] == 1
-            )
-
-        dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
         q = []
-        fresh_o = 0
+        res = 0
 
-        for i in range(ROWS):
-            for j in range(COLS):
-                if grid[i][j] == 2:
-                    q.append([i, j, 0])
-                if grid[i][j] == 1:
-                    fresh_o += 1
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 2:
+                    q.append((r, c, 0))
         
-        max_time = 0
-
+        dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        
         while q:
-            r, c, time = q.pop(0)
-            max_time = max(max_time, time)
+            curr_r, curr_c, curr_t = q.pop(0)
+            res = max(res, curr_t)
 
-            for dx, dy in dirs:
-                new_r, new_c = r+dx, c+dy
-                if is_safe(new_r, new_c):
+            for dr, dc in dirs:
+                new_r, new_c = curr_r + dr, curr_c + dc
+                if 0 <= new_r < ROWS and 0 <= new_c < COLS and grid[new_r][new_c] == 1:
+                    q.append((new_r, new_c, curr_t + 1))
                     grid[new_r][new_c] = 2
-                    fresh_o -= 1
-                    q.append([new_r, new_c, time+1])
         
-        return max_time if fresh_o == 0 else -1
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    return -1
+        
+        return res
