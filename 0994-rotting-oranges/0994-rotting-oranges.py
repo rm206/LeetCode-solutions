035628595ac251e072975d7f85d74fd7605daafa
Rishabh <75585601+rm206@users.noreply.github.com>
2026-04-06 23:24:44 -0500

    Time: 4 ms (35.64%), Space: 19.4 MB (55.77%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -4,11 +4,14 @@ class Solution:
 
         q = []
         res = 0
+        fresh_count = 0
 
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 2:
                     q.append((r, c, 0))
+                if grid[r][c] == 1:
+                    fresh_count += 1
         
         dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
         
@@ -21,10 +24,6 @@ class Solution:
                 if 0 <= new_r < ROWS and 0 <= new_c < COLS and grid[new_r][new_c] == 1:
                     q.append((new_r, new_c, curr_t + 1))
                     grid[new_r][new_c] = 2
+                    fresh_count -= 1
         
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] == 1:
-                    return -1
-        
-        return res
+        return res if fresh_count == 0 else -1
